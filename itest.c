1e8855fba30d081bf11b687acf3c578afd8613bd
Arseniy Belkov <arsfizteh@gmail.com>
2026-03-21 14:24:47 +0300

    custom assert

--- itest.c
+++ itest.c
@@ -5,38 +5,47 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef unsigned char u8;
+
 static const size_t ___ITEST_MAX_TEST_SUITE_SIZE = 256;
+
+struct ___ITEST_Location {
+    const char* file;
+    const int line;
+};
 
 struct ___ITEST_TestBody {
     // void (*body)(void);
     const char* name;
 };
 
+enum ___ITEST_TestStatus : u8 {
+    ___ITEST_TestStatusRunning = 0,
+    ___ITEST_TestStatusSuccess = 1,
+    ___ITEST_TestStatusFailure = 2,
+};
+
 struct ___ITEST_TestSuite {
     size_t count;
     const char* name;
     const char* current_test;
+    enum ___ITEST_TestStatus current_test_status;
     struct ___ITEST_TestBody tests[___ITEST_MAX_TEST_SUITE_SIZE];
 };
 
-enum ___ITEST_Result : unsigned char {
-    ___ITEST_ResultOk = 1,
-    ___ITEST_ResultErr = 0,
-};
-
-static enum ___ITEST_Result ___ITEST_append_test(struct ___ITEST_TestBody test, struct ___ITEST_TestSuite* suite) {\
+static int ___ITEST_append_test(struct ___ITEST_TestBody test, struct ___ITEST_TestSuite* suite) {\
     if (suite->count >= ___ITEST_MAX_TEST_SUITE_SIZE) {
         fprintf(
             stderr, "Test suite can\'t have more than %lu tests, can't append \'%s\' to \'%s\'\n",
             ___ITEST_MAX_TEST_SUITE_SIZE, test.name, suite->name
         );
-        return ___ITEST_ResultErr;
+        return 0;
     }
     suite->tests[suite->count++] = test;
-    return ___ITEST_ResultOk;
+    return 1;
 }
 
-static void print_test_result(const char* suite, const char* test, unsigned char test_status) {
+static void print_test_result(const char* suite, const char* test, u8 test_status) {
     const char* test_status_str = test_status ? "OK" : "FAILED";\
     FILE* stream = 0 ? stdout : stderr;\
     fprintf(stream, "Test %s::%s ... %s\n", suite, test, test_status_str);\
@@ -44,7 +53,7 @@ static void print_test_result(const char* suite, const char* test, unsigned char
 
 #define ITEST_SUITE_BEGIN(itest_test_suite) int main() {\
     pid_t ___ITEST_is_parent_process = -1;\
-    struct ___ITEST_TestSuite ___ITEST_suite_##itest_test_suite = { .tests = {NULL}, .count = 0, .current_test = NULL };\
+    struct ___ITEST_TestSuite ___ITEST_suite_##itest_test_suite = { .tests = {NULL}, .count = 0, .current_test = NULL, .current_test_status = 0 };\
     
 #define ITEST_SUITE_END(itest_test_suite)\
     ___ITEST_SUITE_SUCCEED();\
@@ -78,7 +87,28 @@ static void print_test_result(const char* suite, const char* test, unsigned char
         /* do nothing */ \
     }\
     else /* here goes the code for the test */
-    
+
+#define ASSERT(expr) do {\
+    const char* expression = #expr;\
+    if (!(expr)) {\
+        ___TEST_suite_##itest_test_suite.\
+    }\
+} while (0)
+
+#define ASSERT_EQ(x, y) ASSERT((x) == (y))
+
+#define ASSERT_NE(x, y) ASSERT((x) != (y))
+
+#define ASSERT_GT(x, y) ASSERT((x) > (y))
+
+#define ASSERT_LT(x, y) ASSERT((x) < (y))
+
+#define ASSERT_GE(x, y) ASSERT((x) >= (y))
+
+#define ASSERT_LE(x, y) ASSERT((x) <= (y))
+
+#define __ITEST_HERE() struct ___ITEST_Location { .file = __FILE__, .line = __LINE__ }
 
 #define ___ITEST_SUITE_SUCCEED() return 0
+
 #define ___ITEST_SUITE_FAIL() return 1
